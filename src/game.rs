648116fd5bdd42eda2a0a333ce9b3dff4b5296d1
heydabop <heydabop@gmail.com>
2022-01-23 17:55:37 -0600

    make Game struct

---
+++ src/game.rs
@@ -0,0 +1,240 @@
+use crate::board::Board;
+use crate::color::Color;
+use crate::space::Space;
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode},
+    execute, queue,
+    style::{self, Color as TermColor, Stylize},
+    terminal, Result,
+};
+use std::io::{stdout, Write};
+
+const SPACE_WIDTH: u16 = 5;
+const SPACE_HEIGHT: u16 = 3;
+const MIN_X: u16 = SPACE_WIDTH / 2;
+const MAX_X: u16 = SPACE_WIDTH * 7 - MIN_X;
+const MIN_Y: u16 = SPACE_HEIGHT / 2;
+const MAX_Y: u16 = SPACE_HEIGHT * 7 + MIN_Y;
+
+pub struct Game {
+    board: Board,
+    selected: Option<(u8, u8)>,
+    undoing: bool,
+    quitting: bool,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            board: Board::new(),
+            selected: None,
+            undoing: false,
+            quitting: false,
+        }
+    }
+
+    pub fn with_board(board: Board) -> Self {
+        Self {
+            board,
+            selected: None,
+            undoing: false,
+            quitting: false,
+        }
+    }
+
+    pub fn run_loop(&mut self) -> Result<()> {
+        let mut stdout = stdout();
+
+        queue!(stdout, terminal::Clear(terminal::ClearType::All))?;
+        self.queue_board()?;
+        queue!(
+            stdout,
+            cursor::MoveLeft(1),
+            cursor::SetCursorShape(cursor::CursorShape::Block),
+            cursor::EnableBlinking,
+            cursor::Show,
+            cursor::MoveTo(SPACE_WIDTH / 2, SPACE_HEIGHT * 7 + SPACE_HEIGHT / 2),
+        )?;
+        stdout.flush()?;
+        terminal::enable_raw_mode()?;
+
+        loop {
+            let e = read()?;
+            if let Event::Key(k) = e {
+                let pos = cursor::position()?;
+                match k.code {
+                    KeyCode::Up => {
+                        if pos.1 > MIN_Y {
+                            execute!(stdout, cursor::MoveUp(SPACE_HEIGHT))?;
+                        }
+                    }
+                    KeyCode::Down => {
+                        if pos.1 < MAX_Y {
+                            execute!(stdout, cursor::MoveDown(SPACE_HEIGHT))?;
+                        }
+                    }
+                    KeyCode::Left => {
+                        if pos.0 > MIN_X {
+                            execute!(stdout, cursor::MoveLeft(SPACE_WIDTH))?;
+                        }
+                    }
+                    KeyCode::Right => {
+                        if pos.0 < MAX_X {
+                            execute!(stdout, cursor::MoveRight(SPACE_WIDTH))?;
+                        }
+                    }
+                    KeyCode::Char('z') => {
+                        self.undoing = true;
+                        self.quitting = false;
+                    }
+                    KeyCode::Char('q') => {
+                        self.quitting = true;
+                        self.undoing = false;
+                    }
+                    KeyCode::Char('y') => {
+                        if self.undoing {
+                            self.board.undo_last_move();
+                            self.undoing = false;
+                            self.queue_board()?;
+                            #[allow(clippy::cast_possible_truncation)]
+                            let x = pos.0 as u8;
+                            #[allow(clippy::cast_possible_truncation)]
+                            let y = (7 - pos.1) as u8;
+                            queue!(stdout, cursor::MoveTo(x.into(), (7 - y).into()))?;
+                            stdout.flush()?;
+                        }
+                        if self.quitting {
+                            break;
+                        }
+                    }
+                    KeyCode::Char('n') => {
+                        self.quitting = false;
+                        self.undoing = false;
+                    }
+                    KeyCode::Char(' ') => {
+                        if pos.0 > MAX_X || pos.1 > MAX_Y {
+                            continue;
+                        }
+                        #[allow(clippy::cast_possible_truncation)]
+                        let x = (pos.0 / SPACE_WIDTH) as u8;
+                        #[allow(clippy::cast_possible_truncation)]
+                        let y = 7 - (pos.1 / SPACE_HEIGHT) as u8;
+                        if let Some(s) = self.selected {
+                            if s.0 == x && s.1 == y {
+                                let space = self.board.space(x, y);
+                                let colors = get_term_colors(space);
+                                execute!(
+                                    stdout,
+                                    style::PrintStyledContent(
+                                        space.draw().with(colors.0).on(colors.1)
+                                    ),
+                                    cursor::MoveLeft(1)
+                                )?;
+                                self.selected = None;
+                            } else if x < 8 && y < 8 && self.board.move_piece(s.0, s.1, x, y) {
+                                self.selected = None;
+                                self.board.next_turn();
+
+                                self.queue_board()?;
+                                queue!(
+                                    stdout,
+                                    cursor::MoveTo(
+                                        u16::from(x) * SPACE_WIDTH + (SPACE_WIDTH / 2),
+                                        (7 - u16::from(y)) * SPACE_HEIGHT + (SPACE_HEIGHT / 2)
+                                    )
+                                )?;
+                                stdout.flush()?;
+                            }
+                        } else if x < 8 && y < 8 {
+                            let space = self.board.space(x, y);
+                            if let Some(piece_color) = space.piece_color() {
+                                if piece_color == self.board.turn_color() {
+                                    self.selected = Some((x, y));
+                                    self.queue_space(x, y)?;
+                                    queue!(
+                                        stdout,
+                                        cursor::MoveUp(MIN_Y),
+                                        cursor::MoveLeft(MIN_X + 1)
+                                    )?;
+                                    stdout.flush()?;
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                };
+            }
+        }
+
+        terminal::disable_raw_mode()?;
+        execute!(stdout, cursor::MoveTo(0, 0))?;
+
+        Ok(())
+    }
+
+    fn queue_board(&mut self) -> Result<()> {
+        for y in 0u8..8u8 {
+            for x in 0u8..8u8 {
+                self.queue_space(x, 7 - y)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn queue_space(&self, space_x: u8, space_y: u8) -> Result<()> {
+        let space = self.board.space(space_x, space_y);
+        let highlighted = if let Some(selected) = self.selected {
+            selected.0 == space_x && selected.1 == space_y
+        } else {
+            false
+        };
+        let x = u16::from(space_x) * SPACE_WIDTH;
+        let y = (7 - u16::from(space_y)) * SPACE_HEIGHT;
+        let mut stdout = stdout();
+        let (fg_color, bg_color) = get_term_colors(space);
+        queue!(
+            stdout,
+            cursor::MoveTo(x, y),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            cursor::MoveTo(x, y + 1),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            if highlighted {
+                style::PrintStyledContent(space.draw().with(fg_color).on_green())
+            } else if space.piece().is_some() {
+                style::PrintStyledContent(space.draw().with(fg_color).on_black())
+            } else {
+                style::PrintStyledContent(space.draw().with(fg_color).on(bg_color))
+            },
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            cursor::MoveTo(x, y + 2),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+            style::PrintStyledContent(' '.on(bg_color)),
+        )?;
+
+        Ok(())
+    }
+}
+
+fn get_term_colors(space: &Space) -> (TermColor, TermColor) {
+    let piece_color = match space.piece_color() {
+        Some(Color::White) => TermColor::Grey,
+        Some(Color::Black) => TermColor::DarkGrey,
+        None => TermColor::White,
+    };
+    let space_color = match space.color() {
+        Color::White => TermColor::White,
+        Color::Black => TermColor::Black,
+    };
+    (piece_color, space_color)
+}
